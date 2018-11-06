e1c9f4f44f6ebe450f02f04f8f8e30c0db2aee55
Rhan Miano <rhanmiano29@gmail.com>
2018-11-06 13:17:09 +0800

    Day 1 - Initiate repository, do day 1 challenge

---
+++ js/solutions.js
@@ -0,0 +1,26 @@
+/**
+ * 100 Days Coding Challenge
+ * 
+ * @language Javascript
+ * @author Rhan Miano
+ * @since November 2018
+ * 
+ * Pushing myself to do this 100 Days Coding Challenge.
+ * 
+ * To test each solution, after launching js/index.html,
+ * manually go to browser's console and create an
+ * instance of the class Solutions and call the function
+ * for each item. (TO BE UPDATED)
+ */
+
+
+class Solutions {
+	
+	// Day 1 Use console.log to print a message.
+	fnExecSolution1() {
+		const message = "Hi! Good luck on this journey of yours. Hoping that it would really turn out well and that you can consider yourself a better one after you do the 100th challenge than what you are today.";
+
+		console.log(message);
+	}
+}
+
---
+++ php/index.php
@@ -0,0 +1,14 @@
+<?php
+	require 'solutions.php';
+?>
+<html>
+	<head>
+		<title>JS Practice</title>
+	</head>
+	<body>
+		<?php
+			$solution = new Solutions();
+			$solution->fnExecSolution1();
+		?>
+	</body>
+</html>
